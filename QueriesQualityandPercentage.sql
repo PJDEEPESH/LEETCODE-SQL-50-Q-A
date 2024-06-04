f35adec7a10d1f76b4ca5537d9c7d507a48e1b17
PJDEEPESH <167789969+PJDEEPESH@users.noreply.github.com>
2024-06-04 12:06:31 +0530

    Add files via upload

---
+++ QueriesQualityandPercentage.sql
@@ -0,0 +1,6 @@
+SELECT query_name,
+       ROUND(AVG(rating / position), 2) AS quality,
+       ROUND(AVG(IF(rating < 3, 1, 0)) * 100, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
