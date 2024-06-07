7fee77005e5a32a19693d962719d89ec9efa5fad
PJDEEPESH <167789969+PJDEEPESH@users.noreply.github.com>
2024-06-07 23:29:20 +0530

    Add files via upload

---
+++ ExchangeSeats.sql
@@ -0,0 +1,8 @@
+SELECT ( CASE
+            WHEN id%2 != 0 AND id != counts THEN id+1
+            WHEN id%2 != 0 AND id = counts THEN id
+            ELSE id-1
+        END) AS id, student
+FROM seat, (select count(*) as counts from seat) 
+AS seat_counts
+ORDER BY id ASC
